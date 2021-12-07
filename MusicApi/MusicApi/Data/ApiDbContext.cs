4d86948dd8f133bc7c6c1ec46962e58ab8e91074
Posccis <lkzq33@gmail.com>
2021-12-07 15:28:30 -0300

    feat(main): Connected with the DB and create the CRUD with the HTTPS verbs

---
+++ MusicApi/MusicApi/Data/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
